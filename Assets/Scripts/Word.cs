2468f62fe060d1f086a4016c63de4d6d3ddaa847
Константин <oe-5ye@mail.ru>
2020-05-13 20:54:32 +0300

    Initial commit

---
+++ Assets/Scripts/Word.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Word : MonoBehaviour
+{
+    /// <summary>
+    /// Слово отображено
+    /// </summary>
+    public void OnWordEnter()
+    {
+        if (GamePlay.Single.State == GamePlayState.BeginShowAnswers)
+            Words.Single.NotifyWordEnter();
+    }
+
+    /// <summary>
+    /// Слово скрыто
+    /// </summary>
+    public void OnWordLeave()
+    {
+        if (GamePlay.Single.State == GamePlayState.BeginShowQuestionResultAnimation)
+            Words.Single.NotifyWordLeave();
+    }
+}
